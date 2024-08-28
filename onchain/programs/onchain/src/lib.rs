ec9490d6de03356fbe679d80d089ac1eb868eaf0
James W. Crofts <jcrofts@pobox.com>
2024-08-28 14:55:29 -0700

    Add default Anchor project
    On-chain Solana Program/Smart Contract support

---
+++ onchain/programs/onchain/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("J4S8hv9YmzqTsxpv4PWzn4DwMx6o5k3kCAhyQrmRKXcE");
+
+#[program]
+pub mod onchain {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
