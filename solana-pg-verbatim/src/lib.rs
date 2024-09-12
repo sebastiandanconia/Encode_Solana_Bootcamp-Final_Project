99430a2a3d29cf1d0cfbfed697e50f89cc2ced0c
James W. Crofts <jcrofts@pobox.com>
2024-09-11 22:02:35 -0700

    Add prediction shares to user account
    - PDAs (Program-Derived Accounts), one per unique user wallet, work
    end-to-end.
    - buy() adds shares to user account.
    - Unit tests (Typescript) successfully read balances from user account.
    
    Now that we have a working prototype of PDAs, reworking the ABI and data
    types may be appropriate. Next, we can add the singleton and per-market
    data structures, add client-side view capabilities, and so on.

--- solana-pg-verbatim/src/lib.rs
+++ solana-pg-verbatim/src/lib.rs
@@ -1,5 +1,7 @@
 use anchor_lang::prelude::*;
 
+use std::mem::size_of;
+
 // This is your program's public key and it will update
 // automatically when you build the project.
 declare_id!("2Fr2VCW2fgw7nkfCCBpq67s9J3YCYtmv4TfskJeREAsi");
@@ -7,27 +9,40 @@ declare_id!("2Fr2VCW2fgw7nkfCCBpq67s9J3YCYtmv4TfskJeREAsi");
 #[program]
 mod hello_anchor {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
-        ctx.accounts.new_account.data = data;
-        msg!("Changed data to: {}!", data); // Message will show up in the tx logs
+
+    pub fn buy(ctx: Context<Buy>, num: u64) -> Result<()> {
+        // Deduct payment
+
+        // Credit shares
+        ctx.accounts.user_holdings.a += num;
         Ok(())
     }
+
 }
+
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    // We must specify the space in order to initialize an account.
-    // First 8 bytes are default account discriminator,
-    // next 8 bytes come from NewAccount.data being type u64.
-    // (u64 = 64 bits unsigned integer = 8 bytes)
-    #[account(init, payer = signer, space = 8 + 8)]
-    pub new_account: Account<'info, NewAccount>,
+pub struct Buy<'info> {
+    #[account(init_if_needed,
+    payer = authority,
+    space = size_of::<UserHoldings>() + 8,
+    seeds = [
+            b"user-holdings-v1",
+            authority.key().as_ref()],
+    bump
+    )]
+    pub user_holdings: Account<'info, UserHoldings>,
+
     #[account(mut)]
-    pub signer: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    authority: Signer<'info>,
+
+    system_program: Program<'info, System>,
 }
 
+
 #[account]
-pub struct NewAccount {
-    data: u64
+pub struct UserHoldings {
+    // Holdings
+    a: u64,
+    b: u64,
 }
--- solana-pg-verbatim/tests/anchor.test.ts
+++ solana-pg-verbatim/tests/anchor.test.ts
@@ -1,34 +1,40 @@
 // No imports needed: web3, anchor, pg and more are globally available
 
 describe("Test", () => {
-  it("initialize", async () => {
-    // Generate keypair for the new account
-    const newAccountKp = new web3.Keypair();
 
-    // Send transaction
-    const data = new BN(42);
+  it("buy", async () => {
+    const [userAccount, userAccountBump] =
+      web3.PublicKey.findProgramAddressSync(
+        [Buffer.from("user-holdings-v1"), pg.wallet.publicKey.toBuffer()],
+
+        pg.program.programId
+      );
+
+    console.log(`Derived userAccount = ${userAccount}`);
+
+    // Read account balance
+    let userAccountData = await pg.program.account.userHoldings.fetch(
+      userAccount
+    );
+    console.log(`Beginning account balance: {}`, userAccountData.a.toNumber());
+
+
     const txHash = await pg.program.methods
-      .initialize(data)
+      .buy(new anchor.BN(20))
       .accounts({
-        newAccount: newAccountKp.publicKey,
+        userHoldings: userAccount,
         signer: pg.wallet.publicKey,
         systemProgram: web3.SystemProgram.programId,
       })
-      .signers([newAccountKp])
       .rpc();
+
     console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
 
     // Confirm transaction
     await pg.connection.confirmTransaction(txHash);
 
-    // Fetch the created account
-    const newAccount = await pg.program.account.newAccount.fetch(
-      newAccountKp.publicKey
-    );
-
-    console.log("On-chain data is:", newAccount.data.toString());
-
-    // Check whether the data on-chain is equal to local 'data'
-    assert(data.eq(newAccount.data));
+    // Reread account balance
+    userAccountData = await pg.program.account.userHoldings.fetch(userAccount);
+    console.log(`Final account balance: {}`, userAccountData.a.toNumber());
   });
 });
